4d7e86ff2c0f1a5abb569d9440adc97cb87afd7c
tsubasa <tsubasa.macbook0902@gmail.com>
2024-08-18 03:43:00 +0900

    論理削除用のコード追加

--- Laravel-backend/app/Models/Reservation.php
+++ Laravel-backend/app/Models/Reservation.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reservation extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = [
         'menu_id',
+        'user_id',
+        'status_flags',
+        'reservation_number',
         'reservation_date',
         'reservation_time'
     ];
--- Laravel-backend/app/Models/User.php
+++ Laravel-backend/app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +22,8 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+    // ソフトデリートのために必要なフィールド
+    protected $dates = ['deleted_at'];
 
     /**
      * The attributes that should be hidden for serialization.
