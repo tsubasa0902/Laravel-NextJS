93ceb32448ed719acc3942fa3cf556e5f62c1083
tsubasa <tsubasa.macbook0902@gmail.com>
2024-08-18 18:00:43 +0900

    カテゴリーAPI作成

--- Laravel-backend/app/Http/Controllers/CategoryController.php
+++ Laravel-backend/app/Http/Controllers/CategoryController.php
@@ -3,46 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Requests\CategoryRequest;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $categories = Category::all();
+        return response()->json($categories, 200);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
-        //
+        $validated = $request->validated();
+        Category::create($validated);
+        return response()->json(['message' => '登録が完了しました',], 200, [], JSON_UNESCAPED_UNICODE);
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $reservation = Category::findOrFail($id);
+        return response()->json($reservation, 200);
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(CategoryRequest $request, $id)
     {
-        //
+        $reservation = Category::findOrFail($id);
+        $validated = $request->validated();
+        $reservation->update($validated);
+        return response()->json(['message' => '更新が完了しました',], 200, [], JSON_UNESCAPED_UNICODE);
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $reservation = Category::findOrFail($id);
+        $reservation->delete();
+        return response()->json(['message' => '削除が完了しました'], 200, [], JSON_UNESCAPED_UNICODE);
     }
 }
