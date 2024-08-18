23140180835cd9c9aa67253cb34b04a23ea4a800
tsubasa <tsubasa.macbook0902@gmail.com>
2024-08-18 19:58:08 +0900

    APIのルーティング追加

--- Laravel-backend/routes/api.php
+++ Laravel-backend/routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ScheduleController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('reservations', ReservationController::class);
+Route::apiResource('menus', MenuController::class);
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('schedules', ScheduleController::class);
